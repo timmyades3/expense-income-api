8a984498c2ce2612cd89c388d4e253eb4e608f2d
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-03-28 00:05:26 +0100

    deploy configs

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
