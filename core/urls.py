9eb824f4cd404c9ce27d679761ae8a3f706c5aa6
timmyades3 <oluwatimileyinadesina623@gmail.com>
2024-03-28 00:54:56 +0100

    deploy config

--- core/urls.py
+++ core/urls.py
@@ -53,6 +53,5 @@ urlpatterns = [
     path('userstats/',include('userstats.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
